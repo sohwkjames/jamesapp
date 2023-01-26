cabec6366cf29cfc59bfb54bb445d8068263ee47
James <jamessoh11@gmail.com>
2023-01-26 23:27:24 +0800

    Add cursor pointer

--- components/countdown/StartPauseButton.js
+++ components/countdown/StartPauseButton.js
@@ -3,7 +3,7 @@ export default function StartPauseButton(props) {
   return (
     <div
       onClick={togglePause}
-      className={`w-24 text-2xl bg-white text-black flex justify-center rounded-sm h-12 `}
+      className={`w-24 text-2xl bg-white text-black flex justify-center rounded-sm h-12 cursor-pointer`}
       style={{
         boxShadow: paused ? "rgb(235 235 235) 0px 6px 0px" : "",
         transform: paused ? "" : "translateY(6px)",
